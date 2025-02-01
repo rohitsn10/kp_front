5145df9326f1a1b334e9da1a54233bb475c454f9
Vatsal <vatsal.p@n10tech.com>
2025-02-01 17:18:47 +0530

    file view

--- src/App.jsx
+++ src/App.jsx
@@ -25,12 +25,12 @@ function App() {
   return (
     <div>
 
-        <Routes>
+        {/* <Routes>
           <Route path="/login" element={<Login />} />
 //           <Route path="/" element={<MainLayout><Home/></MainLayout>} />
           {/* {Landbank} */}
-//           <Route path="/landbank" element={<MainLayout><Landbank/></MainLayout>} />
-//           <Route path="/location" element={<MainLayout><Loction/></MainLayout>} />
+{/* //           <Route path="/landbank" element={<MainLayout><Landbank/></MainLayout>} />
+//           <Route path="/location" element={<MainLayout><Loction/></MainLayout>} /> */} 
       <Routes>
         <Route path="/login" element={<Login />} />
 
